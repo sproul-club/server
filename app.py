b24fb6af223bfb2853c48c3c19630fcf7da19ae1
Tejas Shah <tejashah88@gmail.com>
2020-08-23 14:28:49 -0700

    added temporary error handler for mongoengine validation errors

--- app.py
+++ app.py
@@ -29,6 +29,15 @@ def before_request_func():
         if request.method == 'OPTIONS':
             return 'OK'
 
+@app.errorhandler(mongo.errors.ValidationError)
+def handle_mongo_validation_error(ex):
+    return {
+        'status': 'error',
+        'reason': 'A validation error occurred within MongoDB. Please contact the backend dev about this!',
+        'data': [{'field': v_error[0], 'error': v_error[1]} for v_error in ex.to_dict().items()]
+    }, 500
+
+
 app.register_blueprint(user_blueprint)
 app.register_blueprint(catalog_blueprint)
 app.register_blueprint(admin_blueprint)
