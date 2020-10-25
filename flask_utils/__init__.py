3e503c9c398bc37c7ff8a27891f48d113b21f39a
Tejas Shah <tejashah88@gmail.com>
2020-10-25 08:33:01 -0700

    using better slugging method for club, event, and resource ID creation

--- flask_utils/__init__.py
+++ flask_utils/__init__.py
@@ -1,12 +1,16 @@
 __all__ = [
-  'EmailVerifier', 'EmailSender', 'ImageManager', 'PasswordEnforcer',
-  'validate_json', 'id_creator', 'mongo_aggregations'
+    'EmailVerifier', 'EmailSender', 'ImageManager', 'PasswordEnforcer',
+    'validate_json', 'mongo_aggregations', 'role_required',
+    'query_to_objects'
 ]
+
+import json
 
 from flask_utils.email_manager import EmailVerifier, EmailSender
 from flask_utils.image_manager import ImageManager
 from flask_utils.password_enforcer import PasswordEnforcer
 from flask_utils.schema_validator import validate_json
+from flask_utils.role_enforcer import role_required
 from flask_utils import mongo_aggregations
 
-id_creator = lambda string: string.replace(' ', '-').lower()[:100]
+query_to_objects = lambda query: json.loads(query.to_json())
