15dfc69fc620476b6394059470c2ba2bf5e0b295
Tejas Shah <tejashah88@gmail.com>
2021-01-11 15:56:52 -0800

    using user refs for club list storage for now

--- models/student.py
+++ models/student.py
@@ -5,9 +5,9 @@ from models.user import NewBaseUser, USER_ROLES
 from models.metadata import Major, Minor, Tag
 
 class StudentKanbanBoard(gj.EmbeddedDocument):
-    interested_clubs  = mongo.ListField(mongo.StringField(), required=True)
-    applied_clubs     = mongo.ListField(mongo.StringField(), required=True)
-    interviewed_clubs = mongo.ListField(mongo.StringField(), required=True)
+    interested_clubs  = mongo.ListField(mongo.ReferenceField('NewOfficerUser'), default=[])
+    applied_clubs     = mongo.ListField(mongo.ReferenceField('NewOfficerUser'), default=[])
+    interviewed_clubs = mongo.ListField(mongo.ReferenceField('NewOfficerUser'), default=[])
 
     meta = {'auto_create_index': False}
 
@@ -16,12 +16,18 @@ class NewStudentUser(NewBaseUser):
     role = mongo.StringField(default='student', choices=USER_ROLES)
     has_usable_password = mongo.BooleanField(default=False)
 
-    majors = mongo.ListField(mongo.ReferenceField(Major), required=True, max_length=3)
-    minors = mongo.ListField(mongo.ReferenceField(Minor), required=True, max_length=3)
-    interests = mongo.ListField(mongo.ReferenceField(Tag), required=True)
+    registered_on = mongo.DateTimeField(default=datetime.datetime.utcnow)
+    confirmed     = mongo.BooleanField(default=True)
+    confirmed_on  = mongo.DateTimeField(default=datetime.datetime.utcnow)
 
-    favorited_clubs = mongo.ListField(mongo.StringField(), default=[])
-    visited_clubs = mongo.ListField(mongo.StringField(), default=[])
+    full_name = mongo.StringField(max_length=100)
+
+    majors = mongo.ListField(mongo.ReferenceField(Major), max_length=3)
+    minors = mongo.ListField(mongo.ReferenceField(Minor), max_length=3)
+    interests = mongo.ListField(mongo.ReferenceField(Tag))
+
+    favorited_clubs = mongo.ListField(mongo.ReferenceField('NewOfficerUser'), default=[])
+    visited_clubs = mongo.ListField(mongo.ReferenceField('NewOfficerUser'), default=[])
 
     club_board = mongo.EmbeddedDocumentField(StudentKanbanBoard)
 
