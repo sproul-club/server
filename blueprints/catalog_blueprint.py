b09d4c7108764ae0d5cb5e58ad96c224a513b800
Tejas Shah <tejashah88@gmail.com>
2021-01-13 22:58:42 -0800

    added last updated and apply deadline ending to catalog fetching

--- blueprints/catalog_blueprint.py
+++ blueprints/catalog_blueprint.py
@@ -9,7 +9,7 @@ catalog_blueprint = Blueprint('catalog', __name__, url_prefix='/api/catalog')
 CATALOG_VIEW_FIELDS = [
     'club.name', 'club.link_name', 'club.about_us',
     'club.tags', 'club.app_required', 'club.new_members', 'club.num_users',
-    'club.logo_url', 'club.banner_url'
+    'club.logo_url', 'club.banner_url', 'club.last_updated', 'club.apply_deadline_end'
 ]
 
 def to_int_safe(s, default):
