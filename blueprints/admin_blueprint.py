19aa480e80e4f3890a1d880bf7eaf3c20d72377e
Tejas Shah <tejashah88@gmail.com>
2021-01-13 21:48:00 -0800

    fixed not being able to update num of users

--- blueprints/admin_blueprint.py
+++ blueprints/admin_blueprint.py
@@ -70,7 +70,7 @@ def edit_profile():
         if key == 'tags':
             user.club['tags'] = Tag.objects.filter(id__in=json['tags'])
         elif key == 'num_users':
-            user.club['num_users'] = NumUsersTag.objects.filter(id=num_users_id).first()
+            user.club['num_users'] = NumUsersTag.objects.filter(id=json['num_users']).first()
         elif key == 'social_media_links':
             user.update(club__social_media_links=json['social_media_links'])
         else:
