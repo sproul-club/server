8c62e183d9843bd3456aadb587b41f267b5760ab
Tejas Shah <tejashah88@gmail.com>
2021-11-16 22:43:37 -0800

    added documentation for Catalog API

--- blueprints/catalog_blueprint.py
+++ blueprints/catalog_blueprint.py
@@ -17,12 +17,21 @@ CATALOG_VIEW_FIELDS = [
 ]
 
 def to_int_safe(s, default):
+    """
+    A safe string to integer function that returns a default if it fails.
+    """
+
     try:
         return int(s)
     except ValueError:
         return default
 
+
 def _random_generic_club_recommendations(size):
+    """
+    Generates random clubs recommendations, intended for testing purposes only.
+    """
+
     random_recommended_users = NewOfficerUser.objects \
         .filter(confirmed=True) \
         .filter(club__reactivated=True) \
@@ -41,6 +50,10 @@ def _random_generic_club_recommendations(size):
 
 
 def _fetch_recommended_club_info(link_names):
+    """
+    Given a set of clubs' link names, fetch the relevant club information for the club view page.
+    """
+
     random_recommended_users = NewOfficerUser.objects \
         .filter(club__link_name__in=link_names)
 
@@ -59,17 +72,29 @@ def _fetch_recommended_club_info(link_names):
 @catalog_blueprint.route('/tags', methods=['GET'])
 @as_json
 def get_tags():
+    """
+    GET endpoint that fetches the set of club tags.
+    """
+
     return query_to_objects(Tag.objects.all())
 
 
 @catalog_blueprint.route('/num-user-tags', methods=['GET'])
 @as_json
 def get_num_user_tags():
+    """
+    GET endpoint that fetches the set of "# of users" tags.
+    """
+
     return query_to_objects(NumUsersTag.objects.all())
 
 
 @catalog_blueprint.route('/organizations', methods=['GET'])
 def get_organizations():
+    """
+    GET endpoint that fetches the list of organizations without filters, sorted alphabetically.
+    """
+
     limit = to_int_safe( request.args.get('limit'), 50)
     skip = to_int_safe( request.args.get('skip'), 0)
 
@@ -92,6 +117,11 @@ def get_organizations():
 @catalog_blueprint.route('/organizations/<org_link_name>', methods=['GET'])
 @jwt_optional
 def get_org_by_id(org_link_name):
+    """
+    GET endpoint that fetches all information of the requested club organization, including
+    its similarly recommended clubs.
+    """
+
     user = NewOfficerUser.objects(
         club__link_name=org_link_name,
         confirmed=True,
@@ -110,6 +140,7 @@ def get_org_by_id(org_link_name):
     current_user = get_current_user()
 
     if current_user and current_user.role == 'student':
+        # Save the club as part of the student's visiting history of clubs
         current_user.visited_clubs += [org_link_name]
         current_user.save()
 
