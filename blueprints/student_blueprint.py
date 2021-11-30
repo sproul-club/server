ab4ade52405a827c151a165ba9a027cf511f152e
tankaren <karentan@berkeley.edu>
2021-11-30 11:37:11 -0800

    revert back to before

--- blueprints/student_blueprint.py
+++ blueprints/student_blueprint.py
@@ -102,7 +102,6 @@ fa = FlaskAuthomatic(
                 'https://www.googleapis.com/auth/userinfo.profile',
                 'https://www.googleapis.com/auth/userinfo.email'
             ],
-            'url': 'https://sc-backend-dev.herokuapp.com/api/student/finish-register', 
         }
     },
     secret=CurrentConfig.SECRET_KEY,
